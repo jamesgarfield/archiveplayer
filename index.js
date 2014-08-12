48e9432916e53ff948cb959b590e231356c72753
James Garfield <jgarfield@adblocks.com>
2014-08-12 15:21:56 -0400

    Fix routing/history api integration

--- index.js
+++ index.js
@@ -11,16 +11,11 @@ _.mixin({
 		
 		audioPlayer.setContainer("playlistContainer");
 		
-		function goingToTheShow(showID) {
-			var pplaylist = archiveLoader.showByID(showID)
-			pplaylist.then(showPlaylist)
-		}
-
 		var router = Router({
 			"archive/random/artist/:artistName" : archiveLoader.randomShowByCollection,
 			"archive/show/:showID" : goingToTheShow,
-			"playlist" : pageRouter,
-			"results" : pageRouter,
+			"playlist" : locationRouter,
+			"results" : locationRouter,
 		});
 		router.init();
 
@@ -31,8 +26,49 @@ _.mixin({
 				$("#advSearchArtist").autocomplete({source:collections});
 			});
 
+		window.addEventListener('popstate', popStateRouter);
 
-		function pageRouter() {
+		function buildLocation(param, page) {
+			var location = window.location.pathname;
+			var queryParam = [];
+			if (param) {
+				for (col in param)	{
+					queryParam.push([col, param[col]].join('='));
+				}
+			}
+			var query = queryParam.join('&');
+			if (query.length) {
+				location = [location, query].join('?');
+			}
+			if (page) {
+				var delim = page[0] == '#' ? '' : '#';
+				location = [location, page].join(delim);
+			}
+			return location;
+		}
+
+		function setLocation(param, page) {
+			var location = buildLocation(param, page);
+			
+			$.mobile.changePage(page);
+			
+			var currentLocation = [window.location.pathname + window.location.search + window.location.hash].join('') 
+			if (currentLocation != location) {
+				window.history.pushState({
+					params : param,
+					page: page
+				}, page, location);		
+			}
+			
+			return location;
+		}
+
+		function goingToTheShow(showID) {
+			var pplaylist = archiveLoader.showByID(showID)
+			pplaylist.then(showPlaylist)
+		}
+
+		function locationRouter() {
 			var location = window.location;
 			var search = location.search;
 
@@ -46,6 +82,18 @@ _.mixin({
 				params[part[0]] = part[1];
 				return params;
 			}, {})
+			
+		}
+
+		function popStateRouter(event) {
+			var state = event.state;
+			var params = state ? state.params : {};
+			var page = state ? state.page : '#search';
+			baseRouter(params, page);
+		}
+
+		function baseRouter(params, page) {
+			//setLocation(params, page);
 			if (params.show && params.source == "archive") {
 				goingToTheShow(params.show);
 			}
@@ -61,9 +109,10 @@ _.mixin({
 		}
 
 		var showPlaylist = function (playlist) {
-			$.mobile.changePage("#playlist");
-			var location = "/?show=" + playlist.id + "&source=archive#results";
-			window.history.replaceState({}, "", location);
+			var location = setLocation({
+				show : playlist.id, 
+				source: 'archive'
+			}, "#playlist");
 			$("#showTitle").html('<a href="' + location +'" window="_blank">' + playlist.title +"</a>" );
 			$("#originalSource").html('<a href="' + playlist.originalURL +'" window="_blank">Taped by ' + playlist.taper + " @ archive.org</a>" );
 			audioPlayer.loadPlaylist(playlist.files);
@@ -81,9 +130,7 @@ _.mixin({
 				then(function (playlist) {
 					if (playlist.files.length > 0) {
 						$.mobile.loading('hide');
-						//showPlaylist(playlist);
-						var location = "/?show=" + playlist.id + "&source=archive#results";
-						window.location.pushState({}, "", location);
+						showPlaylist(playlist);
 					}
 					else if (attempt < 30){
 						pickRandomShow(collection, (attempt || 0) + 1);
@@ -99,8 +146,10 @@ _.mixin({
 		$("#artistSearchBox").keypress(function (event) {
 			if (event.which == 13) //Enter Key
 			{
-				var searchFunc = _.bind(archiveLoader.randomShowByCollection, archiveLoader, [$("#artistSearchBox").val()]);
-				pickRandomShow(searchFunc);
+				var artist = $("#artistSearchBox").val();
+				if (artist) {
+					loadArtistYearRange(artist);	
+				}
 			}
 		});
 		
@@ -111,27 +160,43 @@ _.mixin({
 
 		$("#artistListBtn").click(function () { 
 			var artist = $("#artistSearchBox").val();
-			window.location = "/?artist=" + artist + "&find=range#results"
-			//loadArtistYearRange(artist);
+			if (artist) {
+				loadArtistYearRange(artist);	
+			}
 		});
 
 		function loadArtistYearRange(artist) {
+			$.mobile.loading('show', {
+				text : "Searching...", 
+				textVisible : true,
+				theme : 'a'
+			});
 			archiveLoader.getCollectionYearRange(artist)
 			.then(function(years) {
 				listArtistYearRange(artist, years);
+				$.mobile.loading('hide');
 			})
 		}
 
 		function loadArtistYear(artist, year) {
+			$.mobile.loading('show', {
+				text : "Searching...", 
+				textVisible : true,
+				theme : 'a'
+			});
 			archiveLoader.getCollectionYear(artist, year)
 				.then(function(shows) {
 					listArtistYear(artist, year, shows);
+					$.mobile.loading('hide');
 				});
 		}
 
 		function listArtistYearRange(artist, years) {
-			$.mobile.changePage("#results");
-			window.history.replaceState({}, "", "/?artist=" + artist + "&find=range#results");
+			var location = setLocation({
+				artist: artist,
+				find : 'range',
+			}, "#results")
+			
 			$("#resultTitle")[0].innerText = [artist, "Years"].join(' ');
 			years.reverse();
 			var list = $("#resultList")[0];
@@ -139,20 +204,28 @@ _.mixin({
 			$(list).empty();
 			years.forEach(function (y) {
 				var li = document.createElement("li");
-				var html = '<a href="/?artist=' + artist + "&year=" + y + '#results">' + y + "</a>";
+				var searchLocation = buildLocation({
+					artist : artist,
+					year: y
+				}, "#results");
+				var html = '<a href="' + searchLocation + '">' + y + "</a>";
 				li.innerHTML = html;
 				list.appendChild(li);
 			});
 			$(list).listview('refresh');
-			/*$(list).on("click", "li", function(event) {
+			$(list).on("click", "li", function(event) {
 				var year = parseInt(this.innerText, 10);
 				loadArtistYear(artist, year);
-			});*/
+				event.preventDefault();
+			});
 		}
 
 		function listArtistYear(artist, year, shows) {
-			$.mobile.changePage("#results");
-			window.history.replaceState({}, "", "/?artist=" + artist + "&year=" + year + "#results");
+			setLocation({
+				artist : artist,
+				year : year
+			}, '#results');
+			
 			$("#resultTitle")[0].innerText = [artist, year].join(' - ');
 			var list = $("#resultList")[0];
 			$(list).off("click", "li");
@@ -160,17 +233,21 @@ _.mixin({
 
 			shows.forEach(function (s) {
 				var li = document.createElement("li");
-				var location = "/?show=" + s.identifier + "&source=archive#results";
+				var showLocation = buildLocation({
+					show: s.identifier,
+					source: 'archive'
+				}, '#results');
 				var title = s.avg_rating ? (s.title + ' (' + s.avg_rating + ')' ) : s.title;
-				li.innerHTML = '<a href="' + location + '">' + title + '</a>';
+				li.innerHTML = '<a href="' + showLocation + '">' + title + '</a>';
 				$(li).attr("data-showid", s.identifier);
 				list.appendChild(li);
 			});
-			$(list).listview('refresh');/*
+			$(list).listview('refresh');
 			$(list).on("click", "li", function(event) {
 				var showID = $(this).attr("data-showid");
 				goingToTheShow(showID);
-			});*/
+				event.preventDefault();
+			});
 		}
 
 		$("#taperSearchBox").keypress(function (event) {
